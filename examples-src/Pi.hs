72ce27e5dfee3a31c09e0b2b401069c45554584b
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2014-06-28 16:39:52 +0200

    Add Criterion benchmarking to Pi example

--- examples-src/Pi.hs
+++ examples-src/Pi.hs
@@ -4,6 +4,7 @@ import Control.Monad (liftM2)
 import Control.Monad.MonteCarlo
 import Data.Summary.Bool
 import System.Random.TF
+import Criterion.Main
 
 mcSquareD :: RandomGen g => MonteCarlo g (Double,Double)
 mcSquareD = liftM2 (,) (randomR (-1,1)) (randomR (-1,1))
@@ -19,6 +20,8 @@ noRuns = 1000000
 main :: IO ()
 main = do
     g <- newTFGen
-    let s = experimentP inUnitCircle noRuns (noRuns `div` 200) g :: BoolSumm
-    let (m,se) = (4*sampleMean s, 4*sampleSE s)
-    putStrLn $ "Pi is probably between " ++ show (m-se,m+se)
+    defaultMain [
+       bench "experimentP (recEval)"
+         $ nf (experimentP inUnitCircle noRuns (noRuns `div` 200)
+                 :: RandomGen g => g -> BoolSumm) g
+     ]
